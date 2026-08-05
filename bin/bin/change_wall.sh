1ea38058a6219ff6ca4106cd7cf00044dfb57ccf
Hermes <hermes@mit.edu>
2026-08-05 19:19:59 +0200

    Added change_wall.sh

---
+++ bin/bin/change_wall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export DISPLAY=:0
+mapfile -t wallpapers < <(ls -1 /home/anon/img/wallpapers)
+export NUMBER=$(ls -1 /home/anon/img/wallpapers | wc -l)
+export RANDWAL=$(echo "$RANDOM % $NUMBER" | bc)
+export FINALWAL=${wallpapers[$RANDWAL]}
+
+feh --bg-scale "/home/anon/img/wallpapers/$FINALWAL"
+xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorDisplayPort-0/workspace0/last-image --set "/home/anon/img/wallpapers/$FINALWAL"
+
