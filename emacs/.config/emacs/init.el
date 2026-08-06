f7c4a512a7a934a85796baac09819f335e05a8d3
Hermes <hermes@mit.edu>
2026-08-06 14:35:20 +0200

    Removed gibberish from init.el

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,17 +1,2 @@
 (setq vc-follow-symlinks t)
 (org-babel-load-file "~/.config/emacs/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-	  '("4ade6b630ba8cbab10703b27fd05bb43aaf8a3e5ba8c2dc1ea4a2de5f8d45882"
-		   default))
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
