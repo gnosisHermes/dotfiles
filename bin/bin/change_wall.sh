f9464f0c4132e9698cd4a29b4ed043e7fbcbcdff
Hermes <hermes@mit.edu>
2026-08-20 12:24:51 +0200

    bin: nuke feh and use xwallpaper instead

--- bin/bin/change_wall.sh
+++ bin/bin/change_wall.sh
@@ -6,6 +6,5 @@ export NUMBER=$(ls -1 /home/anon/img/wallpapers | wc -l)
 export RANDWAL=$(echo "$RANDOM % $NUMBER" | bc)
 export FINALWAL=${wallpapers[$RANDWAL]}
 
-feh --bg-scale "/home/anon/img/wallpapers/$FINALWAL"
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorDisplayPort-0/workspace0/last-image --set "/home/anon/img/wallpapers/$FINALWAL"
+xwallpaper --stretch "/home/anon/img/wallpapers/$FINALWAL"
 
